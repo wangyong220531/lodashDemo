c56078a8af21e311a949af955b21b4f857699dfe
王勇 <wy19945372694@163.com>
2023-02-07 02:15:03 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 import "./App.css"
 
 const App: React.FC = () => {
-    const obj = {
+    const obj = [{
         name: "腕豪",
         age: 22
-    }
+    }, {
+        name: "亚索",
+        age: 23
+    }]
 
     const newObj = _.cloneDeep(obj)
 
     return <>
-        <button onClick={() => { console.log(newObj); }}>点击</button>
+        <button onClick={() => { console.log(newObj[0] === obj[0]); }}>点击</button>
     </>
 }
 
